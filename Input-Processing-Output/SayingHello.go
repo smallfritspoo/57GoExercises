a27b6c9542373ad968128bd74ff1fabfc91a285d
Harry Richardson <richardson.chesney@gmail.com>
2019-02-21 16:32:47 -0600

    Program to say hello to you

---
+++ Input-Processing-Output/SayingHello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func stringCRLFToLF(name string) string {
+	return strings.Replace(name, "\r\n", "", -1)
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hello what is your name?")
+
+	text, _ := reader.ReadString('\n')
+
+	fmt.Printf("Hello, %s, nice to meet you!\n", stringCRLFToLF(text))
+
+}
